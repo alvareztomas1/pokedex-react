9b2ecbf5247eb2a7a3ada4c00deb2a02fac548e8
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-03-31 12:12:29 -0300

    Updates Pagination component
    Updates the component to achieve a responsive pagination. For doing it, uses Bootstrap components such as Container, Row and Column (which will have different sizes depending of the page size)

--- my-app/src/components/Pagination.js
+++ my-app/src/components/Pagination.js
@@ -1,14 +1,29 @@
-const Pagination = ({page, totalPages, onChange, goButtonOnClick, backButtonOnClick, selectedPokemon}) => {
-    return (
-        <div className={selectedPokemon ? "hidden" : "pagination"}>
+import { Button, Col, Container, Row } from "react-bootstrap";
 
-            <button onClick ={backButtonOnClick} className='pagination-button' id="back-button">Back</button>
-            <input type="number" value={page} onChange={onChange} className='pagination-input' id="page-number"></input>
-            <label className='total-pages'>{totalPages}</label>
-            <button onClick={goButtonOnClick} className='pagination-button' id="next-button">Go </button> 
-            
-        </div>
-        
+const Pagination = ({ page, totalPages, onChange, goButtonOnClick, backButtonOnClick, selectedPokemon }) => {
+    return (
+        <>
+            {!selectedPokemon &&
+                <Container id="pagination">
+                    <Row className="justify-content-center justify-content-md-center align-items-center">
+                        <Col xs={2} sm={2} md={2} lg={1}>
+                            <Button onClick={backButtonOnClick} variant="outline-dark" id="back-button">Back</Button>
+                        </Col>
+                        <Col xs={2} sm={2} md={2} lg={2}>
+                            <input type="number" value={page} onChange={onChange} className='form-control text-center' id="page-number"></input>
+                        </Col>
+                        <Col xs={1} sm={1} md={1} lg={1}>
+                            <h4 id="total-pages" className='title'>{totalPages}</h4>
+                        </Col>
+                        <Col xs={2} sm={2} md={2} lg={1}>
+                            <Button variant="outline-dark" onClick={goButtonOnClick} id="next-button">Go </Button>
+                        </Col>
+                    </Row>
+                </Container>
+            }
+        </>
+
+
     );
 };
 
